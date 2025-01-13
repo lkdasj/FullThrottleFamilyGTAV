15c33c241e1f5f64794f99973f906b6728c3129b
ignacio-j3 <ignaciocovarrubias65@gmail.com>
2025-01-13 20:19:02 -0300

    Update

---
+++ gestion-vehiculos/datos-colecciones.js
@@ -0,0 +1,27 @@
+//const vehiculos = [
+//    {
+//        coleccion: 'coleccion1',
+//        datos: [
+//            {nombre: "nombre y modelo", imagen: "ruta de la imagen", inspirado: "de que fue inspirado el vehiculo o donde aparece"},
+//            {nombre: "nombre y modelo", imagen: "ruta de la imagen", inspirado: "de que fue inspirado el vehiculo o donde aparece"}
+//        ]
+//    },
+//    {
+//        coleccion: 'coleccion2',
+//        datos: [
+//            {nombre: "nombre y modelo", imagen: "ruta de la imagen", inspirado: "de que fue inspirado el vehiculo o donde aparece"},
+//            {nombre: "nombre y modelo", imagen: "ruta de la imagen", inspirado: "de que fue inspirado el vehiculo o donde aparece"}
+//        ]
+//    }
+//]
+
+const vehiculos = [
+    {
+        coleccion: 'fast&furius1',
+        datos: [
+            {nombre: "jester classic", imagen: "", inspirado: "mk4 paulwalker"},
+            {nombre: "jester", imagen: "", inspirado: "mk4 paulwalker"},
+            {nombre: "jester", imagen: "", inspirado: "mk4 paulwalker"},
+        ]
+    }
+]
